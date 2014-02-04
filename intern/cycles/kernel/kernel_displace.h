0bb8432b0bd6aa5ce932c3eac441559b4540c216
Dalai Felinto <dfelinto@gmail.com>
2014-02-04 15:13:56 -0200

    Cycles-Bake: remove old code for passes + style touch ups

--- intern/cycles/kernel/kernel_displace.h
+++ intern/cycles/kernel/kernel_displace.h
@@ -28,7 +28,7 @@ ccl_device void compute_light_pass(KernelGlobals *kg, ShaderData *sd, PathRadian
 	path_radiance_init(L, kernel_data.film.use_light_pass);
 
 	/* take multiple samples */
-	for(int sample = 0; sample < samples; sample++) {
+	for (int sample = 0; sample < samples; sample++) {
 		PathRadiance L_sample;
 		PathState state;
 		Ray ray;
@@ -46,7 +46,7 @@ ccl_device void compute_light_pass(KernelGlobals *kg, ShaderData *sd, PathRadian
 		shader_eval_surface(kg, sd, rbsdf, state.flag, SHADER_CONTEXT_MAIN);
 
 		/* sample light and BSDF */
-		if(kernel_path_integrate_lighting(kg, &rng, sd, &throughput, &state, &L_sample, &ray)) {
+		if (kernel_path_integrate_lighting(kg, &rng, sd, &throughput, &state, &L_sample, &ray)) {
 #ifdef __LAMP_MIS__
 			state.ray_t = 0.0f;
 #endif
@@ -228,97 +228,6 @@ ccl_device void kernel_bake_evaluate(KernelGlobals *kg, ccl_global uint4 *input,
 
 		/* extra */
 		case SHADER_EVAL_ENVIRONMENT:
-
-#if 0
-		case SHADER_EVAL_COMBINED:
-		{
-			/* TODO it's not taking into consideration the
-			   RGB inputs of the shader nodes, so it's differing
-			   from the Composite COMBINED */
-
-			int shader;
-			float3 Ng = triangle_normal_MT(kg, prim, &shader);
-
-			shader_setup_from_sample(kg, &sd, P, Ng, I, shader, object, prim, u, v, t, time, bounce, segment);
-			shader_eval_surface(kg, &sd, 0.f, 0, SHADER_CONTEXT_MAIN);
-
-#ifdef __MULTI_CLOSURE__
-			float3 eval = make_float3(0.0f, 0.0f, 0.0f);
-
-			for(int i = 0; i< sd.num_closure; i++) {
-				const ShaderClosure *sc = &sd.closure[i];
-				eval += sc->weight;
-			}
-
-			out = eval;
-#else
-			out = sd.closure.weight;
-#endif
-			break;
-		}
-		case SHADER_EVAL_DIFFUSE:
-		{
-			int shader;
-			float3 Ng = triangle_normal_MT(kg, prim, &shader);
-
-			shader_setup_from_sample(kg, &sd, P, Ng, I, shader, object, prim, u, v, t, time, bounce, segment);
-			shader_eval_surface(kg, &sd, 0.f, 0, SHADER_CONTEXT_MAIN);
-
-#ifdef __MULTI_CLOSURE__
-			float3 eval = make_float3(0.0f, 0.0f, 0.0f);
-
-			for(int i = 0; i< sd.num_closure; i++) {
-				const ShaderClosure *sc = &sd.closure[i];
-				if(sc->type == CLOSURE_BSDF_DIFFUSE_ID)
-					eval += sc->weight;
-			}
-
-			out = eval;
-#else
-			if(sd.closure.type == CLOSURE_BSDF_DIFFUSE_ID)
-				out = sd.closure.weight;
-			else
-				out = make_float3(0.0f, 0.0f, 0.0f);
-#endif
-			break;
-		}
-		case SHADER_EVAL_EMISSION:
-		{
-			int shader;
-			float3 Ng = triangle_normal_MT(kg, prim, &shader);
-
-			shader_setup_from_sample(kg, &sd, P, Ng, I, shader, object, prim, u, v, t, time, bounce, segment);
-			shader_eval_surface(kg, &sd, 0.f, 0, SHADER_CONTEXT_EMISSION);
-
-#ifdef __MULTI_CLOSURE__
-			float3 eval = make_float3(0.0f, 0.0f, 0.0f);
-
-			for(int i = 0; i< sd.num_closure; i++) {
-				const ShaderClosure *sc = &sd.closure[i];
-				if(sc->type == CLOSURE_EMISSION_ID)
-					eval += sc->weight;
-			}
-
-			out = eval;
-#else
-			if(sd.closure.type == CLOSURE_EMISSION_ID)
-				out = sd.closure.weight;
-			else
-				out = make_float3(0.0f, 0.0f, 0.0f);
-#endif
-			break;
-		}
-		case SHADER_EVAL_AO:
-		{
-			/* TODO */
-			break;
-		}
-		case SHADER_EVAL_UV:
-		{
-			/* TODO */
-			break;
-		}
-		case SHADER_EVAL_ENVIRONMENT:
 		{
 			/* setup ray */
 			Ray ray;
@@ -343,18 +252,6 @@ ccl_device void kernel_bake_evaluate(KernelGlobals *kg, ccl_global uint4 *input,
 			out = shader_eval_background(kg, &sd, flag, SHADER_CONTEXT_MAIN);
 			break;
 		}
-		case SHADER_EVAL_NORMAL:
-		{
-			int shader;
-			float3 Ng = triangle_normal_MT(kg, prim, &shader);
-
-			shader_setup_from_sample(kg, &sd, P, Ng, I, shader, object, prim, u, v, t, time, bounce, segment);
-
-			out = sd.N;
-			break;
-		}
-#endif
-
 		default:
 		{
 			/* no real shader, returning the position of the verts for debugging */
