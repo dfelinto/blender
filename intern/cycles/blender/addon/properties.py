87e18d9c266687a9e2c5de3008249c1861ef586f
Dalai Felinto <dfelinto@gmail.com>
2015-12-18 20:38:43 -0200

    From review: doversion for Cycles -- I had to duplicate the properties :/
    I tried (unsuccessfully) to at least have a custom set function for the
    old property (cycles.bake_type), but if I do that, the old property
    value is lost.
    
    I'm open to suggestions, but that's the only way I found to handle
    doversioning. Another option would be to simply set the 'pass_type' to
    combine.

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -483,6 +483,33 @@ class CyclesRenderSettings(bpy.types.PropertyGroup):
         cls.bake_type = EnumProperty(
             name="Bake Type",
             default='COMBINED',
+            description="Deprecated, use bake_pass_type instead",
+            items=(
+                ('COMBINED', "Combined", ""),
+                ('AO', "Ambient Occlusion", ""),
+                ('SHADOW', "Shadow", ""),
+                ('NORMAL', "Normal", ""),
+                ('UV', "UV", ""),
+                ('EMIT', "Emit", ""),
+                ('ENVIRONMENT', "Environment", ""),
+                ('DIFFUSE_DIRECT', "Diffuse Direct", ""),
+                ('DIFFUSE_INDIRECT', "Diffuse Indirect", ""),
+                ('DIFFUSE_COLOR', "Diffuse Color", ""),
+                ('GLOSSY_DIRECT', "Glossy Direct", ""),
+                ('GLOSSY_INDIRECT', "Glossy Indirect", ""),
+                ('GLOSSY_COLOR', "Glossy Color", ""),
+                ('TRANSMISSION_DIRECT', "Transmission Direct", ""),
+                ('TRANSMISSION_INDIRECT', "Transmission Indirect", ""),
+                ('TRANSMISSION_COLOR', "Transmission Color", ""),
+                ('SUBSURFACE_DIRECT', "Subsurface Direct", ""),
+                ('SUBSURFACE_INDIRECT', "Subsurface Indirect", ""),
+                ('SUBSURFACE_COLOR', "Subsurface Color", ""),
+                ),
+            )
+
+        cls.bake_pass_type = EnumProperty(
+            name="Bake Type",
+            default='COMBINED',
             description="Type of pass to bake",
             items=(
                 ('COMBINED', "Combined", ""),
