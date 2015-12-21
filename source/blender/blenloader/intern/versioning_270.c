d102f07119775f8f3f96d51a4ad7889592390f17
Dalai Felinto <dfelinto@gmail.com>
2015-12-21 15:37:15 -0200

    Yet another custom-bake test build - I wish we could build straight from branches ;)

--- source/blender/blenloader/intern/versioning_270.c
+++ source/blender/blenloader/intern/versioning_270.c
@@ -1036,4 +1036,12 @@ void blo_do_versions_270(FileData *fd, Library *UNUSED(lib), Main *main)
 			}
 		}
 	}
+	if (!MAIN_VERSION_ATLEAST(main, 276, 5)) {
+		if (!DNA_struct_elem_find(fd->filesdna, "BakeData", "int", "pass_filter")) {
+			Scene *scene;
+			for (scene = main->scene.first; scene != NULL; scene = scene->id.next) {
+				scene->r.bake.pass_filter = R_BAKE_PASS_FILTER_ALL;
+			}
+		}
+	}
 }
