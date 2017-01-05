b5d41b1d719b9dc739d53d2e045c6ffdca3fafbf
Dalai Felinto <dfelinto@gmail.com>
2017-01-05 16:32:25 +0100

    RNA: bpy.context.scene_collection
    We will need this for UI eventually, and now I need this to update the
    Python add object routine.

--- source/blender/makesrna/intern/rna_context.c
+++ source/blender/makesrna/intern/rna_context.c
@@ -113,6 +113,12 @@ static PointerRNA rna_Context_scene_layer_get(PointerRNA *ptr)
 	return rna_pointer_inherit_refine(ptr, &RNA_SceneLayer, CTX_data_scene_layer(C));
 }
 
+static PointerRNA rna_Context_scene_collection_get(PointerRNA *ptr)
+{
+	bContext *C = (bContext *)ptr->data;
+	return rna_pointer_inherit_refine(ptr, &RNA_SceneCollection, CTX_data_scene_collection(C));
+}
+
 static PointerRNA rna_Context_tool_settings_get(PointerRNA *ptr)
 {
 	bContext *C = (bContext *)ptr->data;
@@ -213,6 +219,11 @@ void RNA_def_context(BlenderRNA *brna)
 	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
 	RNA_def_property_struct_type(prop, "SceneLayer");
 	RNA_def_property_pointer_funcs(prop, "rna_Context_scene_layer_get", NULL, NULL, NULL);
+
+	prop = RNA_def_property(srna, "scene_collection", PROP_POINTER, PROP_NONE);
+	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
+	RNA_def_property_struct_type(prop, "SceneCollection");
+	RNA_def_property_pointer_funcs(prop, "rna_Context_scene_collection_get", NULL, NULL, NULL);
 
 	prop = RNA_def_property(srna, "tool_settings", PROP_POINTER, PROP_NONE);
 	RNA_def_property_clear_flag(prop, PROP_EDITABLE);
