02be4c4e70f13714f906f8d3b0fe7bebf8456bdf
Dalai Felinto <dfelinto@gmail.com>
2017-04-28 16:18:33 +0200

    Cycles support for preview on viewport with core profile
    This upgrade the drawing code to use latest opengl calls.
    Also, it adds a fallback shader for opencolorio.
    
    Reviewers: sergey, brecht
    Subscribers: merwin, fclem
    
    Differential Revision: https://developer.blender.org/D2652

--- intern/cycles/device/device.cpp
+++ intern/cycles/device/device.cpp
@@ -75,8 +75,13 @@ std::ostream& operator <<(std::ostream &os,
 
 Device::~Device()
 {
-	if(!background && vertex_buffer != 0) {
-		glDeleteBuffers(1, &vertex_buffer);
+	if(!background) {
+		if(vertex_buffer != 0) {
+			glDeleteBuffers(1, &vertex_buffer);
+		}
+		if(fallback_shader_program != 0) {
+			glDeleteProgram(fallback_shader_program);
+		}
 	}
 }
 
@@ -98,125 +103,262 @@ void Device::pixels_free(device_memory& mem)
 	mem_free(mem);
 }
 
-void Device::draw_pixels(device_memory& rgba, int y, int w, int h, int dx, int dy, int width, int height, bool transparent,
-	const DeviceDrawParams &draw_params)
+const char *FALLBACK_VERTEX_SHADER =
+"#version 330\n"
+"uniform vec2 fullscreen;"
+"in vec2 texCoord;"
+"in vec2 pos;"
+"out vec2 texCoord_interp;"
+""
+"vec2 normalize_coordinates()"
+"{"
+"	return (vec2(2.0) * (pos / fullscreen)) - vec2(1.0);"
+"}"
+""
+"void main()"
+"{"
+"	gl_Position = vec4(normalize_coordinates(), 0.0, 1.0);"
+"	texCoord_interp = texCoord;"
+"}\n\0";
+
+const char *FALLBACK_FRAGMENT_SHADER =
+"#version 330\n"
+"uniform sampler2D image_texture;"
+"in vec2 texCoord_interp;"
+"out vec4 fragColor;"
+""
+"float linearrgb_to_srgb(float c)"
+"{"
+"	if (c < 0.0031308) {"
+"		return (c < 0.0) ? 0.0 : c * 12.92;"
+"	}"
+"	return 1.055 * pow(c, 1.0 / 2.4) - 0.055;"
+"}"
+""
+"vec4 linearrgb_to_srgb(vec4 col_from)"
+"{"
+"	vec4 col_to;"
+"	col_to.r = linearrgb_to_srgb(col_from.r);"
+"	col_to.g = linearrgb_to_srgb(col_from.g);"
+"	col_to.b = linearrgb_to_srgb(col_from.b);"
+"	col_to.a = col_from.a;"
+"	return col_to;"
+"}"
+""
+"void main()"
+"{"
+"	fragColor = linearrgb_to_srgb(texture(image_texture, texCoord_interp));"
+"}\n\0";
+
+static int bind_fallback_shader(void)
 {
+	GLuint program = 0;
+	program = glCreateProgram();
+
+	struct Shader {
+		const char *source;
+		GLenum type;
+	} shaders[2] = {
+	    {FALLBACK_VERTEX_SHADER, GL_VERTEX_SHADER},
+	    {FALLBACK_FRAGMENT_SHADER, GL_FRAGMENT_SHADER}
+    };
+
+	for (int i = 0; i < 2; i++) {
+		GLuint shader = glCreateShader(shaders[i].type);
+
+		std::string source_str = shaders[i].source;
+		const char *c_str = source_str.c_str();
+
+		glShaderSource(shader, 1, &c_str, NULL);
+		glCompileShader(shader);
+
+		GLint compiled;
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+		if (!compiled) {
+			std::cerr << "Failed to compile:" << std::endl;
+			GLint log_size;
+			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_size);
+			char* log_msg = new char[log_size];
+			glGetShaderInfoLog(shader, log_size, NULL, log_msg);
+			std::cerr << log_msg << std::endl;
+			delete [] log_msg;
+			return 0;
+		}
+
+		glAttachShader(program, shader);
+	}
+
+	/* Link output. */
+	glBindFragDataLocation(program, 0, "fragColor");
+
+	/* Link and error check. */
+	glLinkProgram(program);
+
+	GLint linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		std::cerr << "Shader program failed to link" << std::endl;
+		GLint  log_size;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_size);
+		char* log_msg = new char[log_size];
+		glGetProgramInfoLog(program, log_size, NULL, log_msg);
+		std::cerr << log_msg << std::endl;
+		delete [] log_msg;
+		return 0;
+	}
+
+	return program;
+}
+
+bool Device::bind_fallback_display_space_shader(const float width, const float height)
+{
+	if(fallback_status == FALLBACK_SHADER_STATUS_ERROR) {
+		return false;
+	}
+
+	if(fallback_status == FALLBACK_SHADER_STATUS_NONE) {
+		fallback_shader_program = bind_fallback_shader();
+
+		if (fallback_shader_program == 0) {
+			fallback_status = FALLBACK_SHADER_STATUS_ERROR;
+			return false;
+		}
+
+		glUseProgram(fallback_shader_program);
+		image_texture_location = glGetUniformLocation(fallback_shader_program, "image_texture");
+		if(image_texture_location < 0) {
+			std::cerr << "Shader doesn't containt the 'image_texture' uniform." << std::endl;
+			fallback_status = FALLBACK_SHADER_STATUS_ERROR;
+			return false;
+		}
+
+		fullscreen_location = glGetUniformLocation(fallback_shader_program, "fullscreen");
+		if(fullscreen_location < 0) {
+			std::cerr << "Shader doesn't containt the 'fullscreen' uniform." << std::endl;
+			fallback_status = FALLBACK_SHADER_STATUS_ERROR;
+			return false;
+		}
+	}
+
+	/* Run this every time. */
+	glUseProgram(fallback_shader_program);
+	glUniform1i(image_texture_location, 0);
+	glUniform2f(fullscreen_location, width, height);
+	return true;
+}
+
+void Device::draw_pixels(
+    device_memory& rgba, int y,
+    int w, int h, int width, int height,
+    int dx, int dy, int dw, int dh,
+    bool transparent, const DeviceDrawParams &draw_params)
+{
+	const bool use_fallback_shader = (draw_params.bind_display_space_shader_cb == NULL);
 	pixels_copy_from(rgba, y, w, h);
+
+	GLhalf *data_pointer = (GLhalf*)rgba.data_pointer;
+
+	data_pointer += 4 * y * w;
+
+	GLuint texid;
+	glGenTextures(1, &texid);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F_ARB, w, h, 0, GL_RGBA, GL_HALF_FLOAT, data_pointer);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glEnable(GL_TEXTURE_2D);
 
 	if(transparent) {
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
-	glColor3f(1.0f, 1.0f, 1.0f);
-
-	if(rgba.data_type == TYPE_HALF) {
-		/* for multi devices, this assumes the inefficient method that we allocate
-		 * all pixels on the device even though we only render to a subset */
-		GLhalf *data_pointer = (GLhalf*)rgba.data_pointer;
-		float vbuffer[16], *basep;
-		float *vp = NULL;
-
-		data_pointer += 4*y*w;
-
-		/* draw half float texture, GLSL shader for display transform assumed to be bound */
-		GLuint texid;
-		glGenTextures(1, &texid);
-		glBindTexture(GL_TEXTURE_2D, texid);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F_ARB, w, h, 0, GL_RGBA, GL_HALF_FLOAT, data_pointer);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-		glEnable(GL_TEXTURE_2D);
-
-		if(draw_params.bind_display_space_shader_cb) {
-			draw_params.bind_display_space_shader_cb();
+	GLint shader_program;
+	if(use_fallback_shader) {
+		if (!bind_fallback_display_space_shader(dw, dh)) {
+			return;
 		}
-
-		if(GLEW_VERSION_1_5) {
-			if(!vertex_buffer)
-				glGenBuffers(1, &vertex_buffer);
-
-			glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-			/* invalidate old contents - avoids stalling if buffer is still waiting in queue to be rendered */
-			glBufferData(GL_ARRAY_BUFFER, 16 * sizeof(float), NULL, GL_STREAM_DRAW);
-
-			vp = (float *)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-
-			basep = NULL;
-		}
-		else {
-			basep = vbuffer;
-			vp = vbuffer;
-		}
-
-		if(vp) {
-			/* texture coordinate - vertex pair */
-			vp[0] = 0.0f;
-			vp[1] = 0.0f;
-			vp[2] = dx;
-			vp[3] = dy;
-
-			vp[4] = 1.0f;
-			vp[5] = 0.0f;
-			vp[6] = (float)width + dx;
-			vp[7] = dy;
-
-			vp[8] = 1.0f;
-			vp[9] = 1.0f;
-			vp[10] = (float)width + dx;
-			vp[11] = (float)height + dy;
-
-			vp[12] = 0.0f;
-			vp[13] = 1.0f;
-			vp[14] = dx;
-			vp[15] = (float)height + dy;
-
-			if(vertex_buffer)
-				glUnmapBuffer(GL_ARRAY_BUFFER);
-		}
-
-		glTexCoordPointer(2, GL_FLOAT, 4 * sizeof(float), basep);
-		glVertexPointer(2, GL_FLOAT, 4 * sizeof(float), ((char *)basep) + 2 * sizeof(float));
-
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
-		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-		glDisableClientState(GL_VERTEX_ARRAY);
-
-		if(vertex_buffer) {
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-
-		if(draw_params.unbind_display_space_shader_cb) {
-			draw_params.unbind_display_space_shader_cb();
-		}
-
-		glBindTexture(GL_TEXTURE_2D, 0);
-		glDisable(GL_TEXTURE_2D);
-		glDeleteTextures(1, &texid);
+		shader_program = fallback_shader_program;
 	}
 	else {
-		/* fallback for old graphics cards that don't support GLSL, half float,
-		 * and non-power-of-two textures */
-		glPixelZoom((float)width/(float)w, (float)height/(float)h);
-		glRasterPos2f(dx, dy);
-
-		uint8_t *pixels = (uint8_t*)rgba.data_pointer;
-
-		pixels += 4*y*w;
-
-		glDrawPixels(w, h, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-
-		glRasterPos2f(0.0f, 0.0f);
-		glPixelZoom(1.0f, 1.0f);
+		draw_params.bind_display_space_shader_cb();
+		glGetIntegerv(GL_CURRENT_PROGRAM, &shader_program);
 	}
 
-	if(transparent)
+	if(!vertex_buffer) {
+		glGenBuffers(1, &vertex_buffer);
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	/* invalidate old contents - avoids stalling if buffer is still waiting in queue to be rendered */
+	glBufferData(GL_ARRAY_BUFFER, 16 * sizeof(float), NULL, GL_STREAM_DRAW);
+
+	float *vpointer = (float *)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+
+	if(vpointer) {
+		/* texture coordinate - vertex pair */
+		vpointer[0] = 0.0f;
+		vpointer[1] = 0.0f;
+		vpointer[2] = dx;
+		vpointer[3] = dy;
+
+		vpointer[4] = 1.0f;
+		vpointer[5] = 0.0f;
+		vpointer[6] = (float)width + dx;
+		vpointer[7] = dy;
+
+		vpointer[8] = 1.0f;
+		vpointer[9] = 1.0f;
+		vpointer[10] = (float)width + dx;
+		vpointer[11] = (float)height + dy;
+
+		vpointer[12] = 0.0f;
+		vpointer[13] = 1.0f;
+		vpointer[14] = dx;
+		vpointer[15] = (float)height + dy;
+
+		if(vertex_buffer) {
+			glUnmapBuffer(GL_ARRAY_BUFFER);
+		}
+	}
+
+	GLuint vertex_array_object;
+	GLuint position_attribute, texcoord_attribute;
+
+	glGenVertexArrays(1, &vertex_array_object);
+	glBindVertexArray(vertex_array_object);
+
+	texcoord_attribute = glGetAttribLocation(shader_program, "texCoord");
+	position_attribute = glGetAttribLocation(shader_program, "pos");
+
+	glEnableVertexAttribArray(texcoord_attribute);
+	glEnableVertexAttribArray(position_attribute);
+
+	glVertexAttribPointer(texcoord_attribute, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (const GLvoid *)0);
+	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (const GLvoid *)(sizeof(float) * 2));
+
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+	if(vertex_buffer) {
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	if(use_fallback_shader) {
+		glUseProgram(0);
+	}
+	else {
+		draw_params.unbind_display_space_shader_cb();
+	}
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_TEXTURE_2D);
+	glDeleteTextures(1, &texid);
+
+	if(transparent) {
 		glDisable(GL_BLEND);
+	}
 }
 
 Device *Device::create(DeviceInfo& info, Stats &stats, bool background)
