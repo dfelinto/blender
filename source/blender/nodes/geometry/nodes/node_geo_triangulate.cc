7ff8094a8be8906a4dec53c9cd78f6e81092b90f
Jacques Lucke <jacques@blender.org>
2020-10-22 18:24:05 +0200

    Geometry Nodes: expose minimum vertices input of Triangulate node

--- source/blender/nodes/geometry/nodes/node_geo_triangulate.cc
+++ source/blender/nodes/geometry/nodes/node_geo_triangulate.cc
@@ -26,6 +26,7 @@ Mesh *triangulate_mesh(Mesh *mesh,
 
 static bNodeSocketTemplate geo_node_triangulate_in[] = {
     {SOCK_GEOMETRY, N_("Geometry")},
+    {SOCK_INT, N_("Minimum Vertices"), 4, 0, 0, 0, 4, 10000},
     {-1, ""},
 };
 
@@ -38,11 +39,12 @@ namespace blender::nodes {
 static void geo_triangulate_exec(bNode *UNUSED(node), GValueByName &inputs, GValueByName &outputs)
 {
   GeometryPtr geometry_in = inputs.extract<GeometryPtr>("Geometry");
+  const int min_vertices = std::max(inputs.extract<int>("Minimum Vertices"), 4);
   GeometryPtr geometry_out;
   if (geometry_in.has_value()) {
     Mesh *mesh_in = geometry_in->mesh_get_for_read();
     if (mesh_in != nullptr) {
-      Mesh *mesh_out = triangulate_mesh(mesh_in, 3, 0, 4, 0);
+      Mesh *mesh_out = triangulate_mesh(mesh_in, 3, 0, min_vertices, 0);
       geometry_out = GeometryPtr{new Geometry()};
       geometry_out->mesh_set_and_transfer_ownership(mesh_out);
     }
